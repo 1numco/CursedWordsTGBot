e31d5e526575346c37de98d97cfac8bd5e857198
Jonny251989 <mailbox.lazarev@yandex.ru>
2025-01-20 21:38:44 +0300

    third commit in CI branch

--- build-local.sh
+++ build-local.sh
@@ -6,7 +6,9 @@ cd $project_directory
 
 mkdir -p build
 cd build
-cmake ..
+: ${TGBOT_CPP_INSTALL_PREFIX:=""}
+
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=./build -DTGBOT_CPP_INSTALL_PREFIX=$TGBOT_CPP_INSTALL_PREFIX
 make -j$(nproc)
 
 mkdir -p ../bins/CursedWordsTGBot
