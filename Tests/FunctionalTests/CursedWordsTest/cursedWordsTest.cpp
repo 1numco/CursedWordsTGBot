179c62247ceb958409250ce8ca2871656a065ed1
Jonny251989 <mailbox.lazarev@yandex.ru>
2025-06-02 15:50:46 +0300

    checking 1

--- Tests/FunctionalTests/CursedWordsTest/cursedWordsTest.cpp
+++ Tests/FunctionalTests/CursedWordsTest/cursedWordsTest.cpp
@@ -20,21 +20,18 @@ void ReactorResultTest::generator(){
     std::ifstream inputFile(filePath);
     
     if (!inputFile) {
-        std::cerr << "Не удалось открыть файл!" << std::endl;
+        Logger::getInstance().logInfo(Logger::Levels::Critical, "Не удалось открыть файл!\n");
     }
     std::string line;
     while (std::getline(inputFile, line)) {
         size_t last_space = line.find_last_of(' ');
-        if (last_space == std::string::npos) {
-            std::cerr << "Некорректный формат строки: " << line << std::endl;
-            continue;
-        }
         std::string flag_str = line.substr(last_space + 1);
         bool flag = (flag_str == "1");
+        std::string clean_line = line.substr(0, last_space);
+        message_container[clean_line] = flag;
+        t_bot->getApi().sendMessage(chat_id_, clean_line);
 
-        message_container[line] = flag;
-        std::cout << "sendMessage : " << line << "\n";
-        t_bot->getApi().sendMessage(chat_id_, line);
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
     inputFile.close();
 }
@@ -45,30 +42,29 @@ void ReactorResultTest::checker() {
 
     t_bot->getEvents().onAnyMessage([&](TgBot::Message::Ptr message) {
 
-        std::cout << "message before : " << message->replyToMessage->text << "\n";
+        bool react_m = (message->text == "мат!");
+        
+        ASSERT_EQ(message_container[message->replyToMessage->text], react_m);
 
-        if (message->replyToMessage && message_container.count(message->replyToMessage->text)) {
-            bool react_m = (message->text == "мат!");
-            std::cout << "message: [" << message->replyToMessage->text << "]\n";
-            std::cout << "label: " << (message_container[message->replyToMessage->text] ? "[ мат ]": "[ не мат ]" ) << "\n";
-            std::cout << "classifier result: [" << message->text << "]\n";
-            ASSERT_EQ(message_container[message->replyToMessage->text], react_m);
-            count_recieve_messages++;
-            last_change_time = std::chrono::steady_clock::now();
-        } else {
-            std::cout << "Ignored unrelated message: " << message->text << "\n";
-        }
+        count_recieve_messages++;
+        Logger::getInstance().logInfo(Logger::Levels::Info, "count_recieve_messages: " + std::to_string(count_recieve_messages) + "\n");
+
+        last_change_time = std::chrono::steady_clock::now();
     });
 
     try {
-        TgBot::TgLongPoll longPoll(*t_bot, 1);
+        TgBot::TgLongPoll longPoll(*t_bot);
 
         while (count_recieve_messages < limit_sent_messages_ && elapsed_seconds.count() < limit_time_in_sec) {
+
             longPoll.start();
+
             elapsed_seconds = std::chrono::steady_clock::now() - last_change_time;
+            Logger::getInstance().logInfo(Logger::Levels::Info, "elapsed: " + std::to_string(elapsed_seconds.count()) + "\n");
         }
+
     } catch (TgBot::TgException& e) {
-        std::cerr << "error: " << e.what() << "\n";
+        Logger::getInstance().logInfo(Logger::Levels::Critical, std::string("Error") + e.what());
     }
 }
 
@@ -82,5 +78,6 @@ TEST_F(ReactorResultTest, FirstTest) {
     std::this_thread::sleep_for(std::chrono::seconds(2));
 
     std::raise(SIGINT);
+
     checker();
 }
