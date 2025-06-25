9b6b2855964d653103bfad9ab61c69ec03e40d48
Jonny251989 <mailbox.lazarev@yandex.ru>
2025-06-25 14:49:15 +0300

    try run grpc server

--- docker-compose-run-tests.sh
+++ docker-compose-run-tests.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+set -e
 
 docker compose up -d running_bot
+sleep 10  # Дать время боту инициализироваться
 
-docker compose run --rm generator  # Ожидаем завершения
-
-RUNNING_BOT_CONTAINER=$(docker compose ps -q running_bot)
-docker kill --signal=SIGINT $RUNNING_BOT_CONTAINER
+docker compose run --rm generator
+sleep 15  # Важно: дать время боту обработать сообщения!
 
 echo "Запуск checker..."
 docker compose run --rm checker
 
-echo "Остановка всех сервисов..."
+# Остановка после проверки
+RUNNING_BOT_CONTAINER=$(docker compose ps -q running_bot)
+docker kill --signal=SIGINT $RUNNING_BOT_CONTAINER
 docker compose down
