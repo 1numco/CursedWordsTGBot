61d6ad3e03169984638a88f83d859e6b0fec73ba
Jonny251989 <mailbox.lazarev@yandex.ru>
2025-06-25 15:37:51 +0300

    try1

--- docker-compose-run-tests.sh
+++ docker-compose-run-tests.sh
@@ -4,15 +4,8 @@ set -e
 echo "Запуск running_bot..."
 docker compose up -d running_bot
 
-# Ожидание готовности сервера и бота
-echo "Ожидание инициализации (30 секунд)..."
-sleep 5
-
 echo "Запуск generator..."
 docker compose run --rm generator
-
-echo "Ожидание обработки сообщений (20 секунд)..."
-sleep 5
 
 echo "Запуск checker..."
 docker compose run --rm checker
