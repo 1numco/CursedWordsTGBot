5bfdd0d09a9014cdf62ee1b5c3630c4b17eef7a7
Jonny251989 <mailbox.lazarev@yandex.ru>
2025-07-31 23:09:17 +0300

    adding in pull request

--- Tests/FunctionalTests/CursedWordsTest/checker.cpp
+++ Tests/FunctionalTests/CursedWordsTest/checker.cpp
@@ -7,7 +7,15 @@ void Checker::TearDown() {
 void Checker::SetUp() {
     const char* token_of_checker = std::getenv("TELEGRAM_TOKEN_CHECKER");
     const char* chat_id_value = std::getenv("CHAT_ID");
-    chat_id_ = std::stoll(chat_id_value);
+    if (!token_of_checker || !chat_id_value ) {
+        throw std::runtime_error("Invalid TELEGRAM_TOKEN_CHECKER or CHAT_ID!");
+    }
+    try{
+        chat_id_ = std::stoll(chat_id_value);
+    } catch (const std::exception& e) {
+        throw std::runtime_error("Invalid CHAT_ID format: " + std::string(e.what()));
+    }
+
     t_bot_checker = std::make_shared<TgBot::Bot>(token_of_checker);
     count_recieve_messages = 0;
     fill_map();
--- Tests/FunctionalTests/CursedWordsTest/generator.cpp
+++ Tests/FunctionalTests/CursedWordsTest/generator.cpp
@@ -3,10 +3,18 @@
 
 Generator::Generator(){
     const char* token_of_generator_ = std::getenv("TELEGRAM_TOKEN_GENERATOR");
+    const char* chat_id_value = std::getenv("CHAT_ID");
+    if (!token_of_generator_ || !chat_id_value ) {
+        throw std::runtime_error("Invalid TELEGRAM_TOKEN_GENERATOR or CHAT_ID!");
+    }
+    try{
+        chat_id_ = std::stoll(chat_id_value);
+    }catch (const std::exception& e) {
+        throw std::runtime_error("Invalid CHAT_ID format: " + std::string(e.what()));
+    }
     t_bot = std::make_shared<TgBot::Bot>(token_of_generator_);
     count_recieve_messages = 0;
-    const char* chat_id_value = std::getenv("CHAT_ID");
-    chat_id_ = std::stoll(chat_id_value);
+    
 }
 
 void Generator::generator() {
